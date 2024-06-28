fc99bf3a1f1b101768b3b81970f65c02041438e1
Oscar <odmarulandab@unal.edu.co>
2024-06-28 16:17:55 -0500

    optimizar la forma en que se asigna el texto a un elemento por medio de una funcion

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,9 +1,11 @@
-let titulo = document.querySelector('h1');
-let parrafo = document.querySelector(".texto__parrafo"); 
-
-titulo.innerHTML = "Número secreto"
-parrafo.innerHTML = 'Escoge un número del 1 al 10'
 
 function intentoDeUsuario(){
     alert("hi")
 }
+
+function asignarTextoElemento(elem, innerText){
+    document.querySelector(elem).innerHTML = innerText;
+}
+
+asignarTextoElemento("h1", "Número secreto")
+asignarTextoElemento('.texto__parrafo', 'Escoge un número del 1 al 10')
