edf6a49482c441eedcb9fef41f43ce40bb1731fd
Oscar <odmarulandab@unal.edu.co>
2024-06-28 15:15:30 -0500

    agregar archivos para segunda parte de ejercicio numero secreto

---
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+let parrafo = document.querySelector(".texto__parrafo"); 
+
+titulo.innerHTML = "Número secreto"
+parrafo.innerHTML = 'Escoge un número del 1 al 10'
+
+function intentoDeUsuario(){
+    alert("hi")
+}
