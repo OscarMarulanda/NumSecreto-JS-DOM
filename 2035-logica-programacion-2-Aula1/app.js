a1581c630955f48517f54ec87b7b3d30abd7f719
Oscar <odmarulandab@unal.edu.co>
2024-06-28 21:26:54 -0500

    agregar más funciones y condiciones al juego

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,11 +1,39 @@
+let numeroSecreto = generarNumeroSecreto();
+let intentos = 1;
 
-function intentoDeUsuario(){
-    alert("hi")
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('.texto__parrafo', `Acertaste el número en ${intentos} ${intentos == 1 ? "intento" : "intentos"}`)
+    } else {
+        //el usurio no acertó
+        document.querySelector('#valorUsuario').value = ""
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('.texto__parrafo', 'El número secreto es menor')
+        } else {
+            asignarTextoElemento('.texto__parrafo', 'El número secreto es mayor')
+        }
+        intentos++
+    }
+
 }
 
-function asignarTextoElemento(elem, innerText){
+function asignarTextoElemento(elem, innerText) {
     document.querySelector(elem).innerHTML = innerText;
 }
 
 asignarTextoElemento("h1", "Número secreto")
-asignarTextoElemento('.texto__parrafo', 'Escoge un número del 1 al 10')
+asignarTextoElemento('.texto__parrafo', 'Escoge un número del 1 al 10.')
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+/*let numSecreto;
+
+function nuevoNumeroSecreto(){
+    numSecreto = Math.floor(Math.random() * 10) + 1;
+    console.log(numSecreto)
+}*/
+
